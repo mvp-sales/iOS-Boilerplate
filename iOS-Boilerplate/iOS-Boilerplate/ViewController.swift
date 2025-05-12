d3f0994f820c2c7494353e51c6d5b5ab2d52c392
Marcus Sales <old.marcussales@gmail.com>
2025-05-12 10:48:38 +0100

    Initial UIKit files

---
+++ iOS-Boilerplate/iOS-Boilerplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Boilerplate
+//
+//  Created by Marcus Vinicius Palassi Sales on 12/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
