ff2dd0b16c85ecad8c9e517936fbd8dc89a3b65e
Marcus Sales <old.marcussales@gmail.com>
2025-05-12 10:51:14 +0100

    Initial SwiftUI files

---
+++ iOS-Boilerplate/iOS-Boilerplate/iOS_BoilerplateApp.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_BoilerplateApp.swift
+//  iOS-Boilerplate
+//
+//  Created by Marcus Vinicius Palassi Sales on 12/05/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iOS_BoilerplateApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
